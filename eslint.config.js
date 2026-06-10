55b7dd0e679ec58db0e26282b54b74276a5ec919
Joshua Harms <joshua@nozzlegear.com>
2026-06-10 17:13:02 -0500

    Add ESLint and Prettier for linting/formatting
    Configured ESLint with TypeScript support and Prettier for consistent
    style and code formatting.
    
    type: chore
    scope: tooling

---
+++ eslint.config.js
@@ -0,0 +1,23 @@
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import prettier from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    plugins: {
+      prettier: prettier,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    },
+  },
+  prettierConfig,
+  {
+    ignores: ['dist/**', 'node_modules/**', 'bin/**'],
+  },
+);
